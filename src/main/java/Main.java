8a70e5abdb4c2881c981c156133b9d1889c441a7
ArthurZyabirov <82753926+ArthurZyabirov@users.noreply.github.com>
2021-11-30 02:21:35 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server(64);
         server.serverStart(9999);
-        List<NameValuePair> params = server.getQueryParams("https://stackoverflow.com/questions/43602883/how-to-parse-query-param-effectively-in-java");
+        List<NameValuePair> params = server.getQueryParams("https://www.google.com/search?q=netology+ru&sxsrf=AOaemvKyC1kGkaS6Dc7Nu0MTN-XvGdCFeg%3A1638227328253&ei=gF2lYbL1DoOarwTtyLaYDw&ved=0ahUKEwiytdiv2L70AhUDzYsKHW2kDfMQ4dUDCA4&uact=5&oq=netology+ru&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yCQgAEMkDEBYQHjIFCAAQgAQyAggmOgcIABBHELADOgQIIxAnOggIABCABBCxAzoLCAAQgAQQsQMQgwE6BwgAELEDEEM6BAgAEEM6CggAELEDEIMBEEM6BwgjEOoCECc6CgguEMcBEKMCECc6DgguEIAEELEDEMcBEKMCOg4ILhCABBCxAxDHARDRAzoICAAQsQMQgwE6BQguEIAEOggILhCABBCxAzoLCC4QgAQQsQMQgwE6DQguELEDEMcBENEDEEM6DgguEIAEELEDEMcBEK8BOgoILhDHARCjAhBDOgsILhCABBDHARCvAToKCAAQgAQQhwIQFEoECEEYAFCBCVifL2C0M2gKcAJ4AIABVogBjgqSAQIxOJgBAKABAbABCsgBCMABAQ&sclient=gws-wiz");
         for (NameValuePair param : params) {
             System.out.println(param.getName() + " : " + param.getValue());
         }
